f10ad2593d28a866819896e37e9bbcffd422c54f
Vladislav Hilko <uladzislau.hilko@gmail.com>
2022-02-10 18:32:44 -0800

    Install Draggable lib

--- app/javascript/components/CheckListItems.jsx
+++ app/javascript/components/CheckListItems.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { VStack } from '@chakra-ui/react';
+import { VStack, Box } from '@chakra-ui/react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import CheckListItem from './CheckListItem';
 
 const CheckListItems = ({ items, toggleItem, onUpdate }) => {
   return (
-    <VStack spacing={2} w="full">
-      {items.map(item => (
-        <CheckListItem key={item.id} item={item} onToggle={toggleItem} onUpdate={onUpdate} />
-      ))}
-    </VStack>
+    <Droppable droppableId="checklistItems">
+      {(provided) => (
+        <VStack {...provided.droppableProps} ref={provided.innerRef} spacing={2} w="full">
+          {items.map((item, index) => (
+            <Draggable key={item.id} draggableId={String(item.id)} index={index}>
+              {(provided, snapshot) => (
+                <Box
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  w="full"
+                  _hover={{ bg: snapshot.isDragging ? "blue.50" : "" }}
+                >
+                  <CheckListItem item={item} onToggle={toggleItem} onUpdate={onUpdate} />
+                </Box>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </VStack>
+      )}
+    </Droppable>
   );
 };
 
