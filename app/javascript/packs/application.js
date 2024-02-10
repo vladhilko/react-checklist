920e05e6d0a62b17bf712b01c431832091e5def7
Vladislav Hilko <uladzislau.hilko@gmail.com>
2024-02-10 14:42:47 +0100

    Add Basic Check List React Component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,8 @@
 // const images = require.context('./images', true)
 // const imagePath = (name) => images(name, true)
 // Support component names relative to this directory:
+import "../stylesheets/application.scss"
+
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
